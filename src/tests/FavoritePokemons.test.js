f074ee270ba8b87a354b21f36698f0ec51a18200
Renan Storrer <storrerrenan@gmail.com>
2022-09-23 01:06:04 -0300

    Testes FavoritesPokemons.js

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../pages/FavoritePokemons';
+
+test('Teste se é exibida na tela a mensagem No favorite pokemon found', () => {
+
+  renderWithRouter(<FavoritePokemons />);
+  const noFavorite = screen.getByText(/no favorite pokemon found/i);
+  expect(noFavorite).toBeInTheDocument();
+});
